84c52892e3f036969bb3938468a2b467e84a24a7
drakonc <drakonc@gmail.com>
2020-11-27 16:54:30 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-vgbobr
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-vgbobr)
---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { Router, Link } from "@reach/router";
+
+const App = () => (
+  <div>
+    <nav>
+      <Link to="/">Home</Link> <Link to="dashboard">Dashboard</Link>
+    </nav>
+    <Router>
+      <Home path="/" />
+      <Dashboard path="dashboard" />
+    </Router>
+  </div>
+);
+
+const Home = () => (
+  <div>
+    <h2>Welcome</h2>
+  </div>
+);
+
+const Dashboard = () => (
+  <div>
+    <h2>Dashboard</h2>
+  </div>
+);
+
+export default App;
